81f6c3c1434318d1ccee35a8ead4cc8b0e30f32a
Barry Vredevoort <bvredevoort@apro.nl>
2024-12-11 10:45:30 +0100

    added

---
+++ src/main/java/nl/logius/resource/HelloResource.java
@@ -0,0 +1,16 @@
+package nl.logius.resource;
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/Hello")
+@Produces("text/plain")
+public class HelloResource {
+
+    @GET
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
